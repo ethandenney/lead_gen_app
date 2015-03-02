cb57043af88028a2ba50dfff7902cbbeee92f567
Prelang Builder <builder@prelang.com>
2015-03-02 22:36:27 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -1,4 +1,8 @@
 class LeadsController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_lead, only: [:show, :edit, :update, :destroy]
 
   # GET /leads
